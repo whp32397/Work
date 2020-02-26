bdd3d90a87cd358210434d4f313e413bab7f4f11
whp <798766028@qq.com>
2020-02-26 13:08:08 +0800

    "2.26"

---
+++ src/main/java/com/whp/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.whp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class MyController {
+    @RequestMapping("/test")
+    public String test() {
+        return "hello";
+    }
+}
